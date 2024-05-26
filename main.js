4780d97d1ee72d53780adeaae9325d9feb8a5d72
cbotros12345 <134175938+cbotros12345@users.noreply.github.com>
2024-05-26 18:30:35 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function next1()
+{
+    window.location = "airpods.html";
+}
+function next2()
+{
+    window.location = "pc.html";
+}
+function next3()
+{
+    window.location = "bottle.html";
+}
+function next4()
+{
+    window.location = "mouse.html";
+}
+function next5()
+{
+    window.location = "pencil.html";
+}
